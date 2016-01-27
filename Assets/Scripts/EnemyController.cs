179fee526af5b2940acfad6f68ec6d4b3ef63830
unknown-user <unknown-user@fe80::a577:800e:e8e2:5f5%21>
2016-01-27 22:17:38 +1100

    Add damage to projectiles
    Add basic AI to enemy

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -1,28 +1,66 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class EnemyController : MonoBehaviour {
+public class EnemyController : MonoBehaviour
+{
 
-    float currentHealth = 100.0f;
-    float maxHealth = 100.0f;
-    float healthBarLength = 0.0f;
+	float currentHealth = 100.0f;
+	float maxHealth = 100.0f;
+	float healthBarLength = 0.0f;
+	public float enemySightDistance = 10.0f;
+	public float maxAttackDistance = 2.5f;
+	public float rotationSpeed = 2.5f;
+	public float mvSpeed = 1.0f;
+	Transform myPosition;
+	Transform target;
 	// Use this for initialization
-	void Start () {
-        healthBarLength = Screen.width / 2;
+
+	void Awake ()
+	{
+		myPosition = transform;
+	}
+	void Start ()
+	{
+		healthBarLength = Screen.width / 2;
+		target = GameObject.FindGameObjectWithTag ("Player").transform;
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+	{
+		watchForPlayer ();
 	}
 
-    void OnGUI()
-    {
-        GUI.Box(new Rect(10, 10, healthBarLength, 20), currentHealth + "/" + maxHealth);
-    }
+	void OnGUI ()
+	{
+		GUI.Box (new Rect (10, 10, healthBarLength, 20), currentHealth + "/" + maxHealth);
+	}
 
-    public void AdjustCurrentHealth(int adjustment){
-        currentHealth += adjustment;
-        currentHealth = Mathf.Clamp(currentHealth, 0, maxHealth);
-        healthBarLength = (Screen.width / 2) * (currentHealth / maxHealth);
-    }
+	public void AdjustCurrentHealth (int adjustment)
+	{
+		currentHealth += adjustment;
+		currentHealth = Mathf.Clamp (currentHealth, 0, maxHealth);
+		healthBarLength = (Screen.width / 2) * (currentHealth / maxHealth);
+	}
+
+	public void watchForPlayer ()
+	{
+		Vector3 direction = (target.transform.position - transform.position).normalized;
+		float dotDirection = Vector3.Dot (direction, transform.forward);
+		Debug.Log ("distance: " + Vector3.Distance (target.position, myPosition.position));
+		if (Vector3.Distance (target.position, myPosition.position) < enemySightDistance) {
+			playerDetected ();
+		}
+	}
+
+	public void playerDetected ()
+	{
+		Debug.DrawLine (target.position, myPosition.position, Color.red);
+
+		myPosition.rotation = Quaternion.Slerp (myPosition.rotation, Quaternion.LookRotation (target.position - myPosition.position), rotationSpeed * Time.deltaTime);
+		if (Vector3.Distance (target.position, myPosition.position) > maxAttackDistance) {
+			myPosition.position += myPosition.forward * mvSpeed * Time.deltaTime;
+		}
+
+	}
 }
--- Assets/Scripts/projectileController.cs
+++ Assets/Scripts/projectileController.cs
@@ -1,50 +1,55 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class projectileController : MonoBehaviour {
+public class projectileController : MonoBehaviour
+{
 
-    float lifespan = 3.0f;
-    float targetAlpha = 1.0f;
+	float lifespan = 3.0f;
+	float targetAlpha = 1.0f;
 	// Use this for initialization
-	void Start () {
+	void Start ()
+	{
 	    
 	}
 	
 	// Update is called once per frame
-	void Update () {
-        lifespan -= Time.deltaTime;
+	void Update ()
+	{
+		lifespan -= Time.deltaTime;
 
-        if (lifespan <= 0)
-        {
-            Disappear();
-        }
+		if (lifespan <= 0) {
+			Disappear ();
+		}
 	}
 
-    void OnCollisionEnter()
-    {
-       
-    }
+	void OnCollisionEnter (Collision c)
+	{
+		if (c.gameObject.tag == "Enemy") {
+			c.gameObject.GetComponent<EnemyController> ().AdjustCurrentHealth (-10);  
+		}
+	}
 
-    void Disappear(){
-        float fadeTime = 0.0f;
 
-        Color c = gameObject.GetComponent<Renderer>().material.color;
-        float currentAlpha = c.a;
-        int count = 0;
-        while (fadeTime <= 1)
-        {
-            c.a = Mathf.Lerp(currentAlpha, targetAlpha, fadeTime);
-            gameObject.GetComponent<Renderer>().material.color= c;
-            fadeTime += Time.deltaTime / lifespan;
-            Debug.Log(fadeTime);
-            count++;
-            if (count == 10)
-            {
-                break;
-            }
-        }
-        c.a = targetAlpha;
-        gameObject.GetComponent<Renderer>().material.color = c;
-        Destroy(gameObject);
-    }
+
+	void Disappear ()
+	{
+		float fadeTime = 0.0f;
+
+		Color c = gameObject.GetComponent<Renderer> ().material.color;
+		float currentAlpha = c.a;
+		int count = 0;
+		while (fadeTime <= 1) {
+			c.a = Mathf.Lerp (currentAlpha, targetAlpha, fadeTime);
+			gameObject.GetComponent<Renderer> ().material.color = c;
+			fadeTime += Time.deltaTime / lifespan;
+			Debug.Log (fadeTime);
+			count++;
+			if (count == 10) {
+				break;
+			}
+		}
+		c.a = targetAlpha;
+		gameObject.GetComponent<Renderer> ().material.color = c;
+		Destroy (gameObject);
+	}
 }
