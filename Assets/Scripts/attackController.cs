1fe8ccf68ddad143ca6b96623aa28bc8d3a5078c
Paul Mansfield <pmansfield@gracenote.com>
2016-01-27 16:26:39 +1100

    Add the basis for an enemy controller Migrate shootController to attackController so there can be different types of attacks Implement hitting an enemy and taking down its health.

---
+++ Assets/Scripts/attackController.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class attackController : MonoBehaviour
+{
+    public GameObject bullet_prefab;
+    public float bulletSpeed = 100f;
+    public string weaponType = "ranged";
+    List<GameObject> nearEnemy = new List<GameObject>();
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1") )
+        {
+            if (weaponType == "ranged")
+            {
+
+                GameObject bullet = (GameObject)Instantiate(bullet_prefab, Camera.main.transform.position + Camera.main.transform.forward, Camera.main.transform.rotation);
+                bullet.GetComponent<Rigidbody>().AddForce(Camera.main.transform.forward * bulletSpeed, ForceMode.Impulse);
+            }
+            else if (weaponType == "melee")
+            {
+                Debug.Log("Melee Attack");
+                Vector3 playerPos = transform.position;
+                for (int i = 0; i < nearEnemy.Count; i++)
+                {
+                    Vector3 enemyPos = nearEnemy[i].transform.position;
+                    Vector3 enemyDirection = enemyPos - playerPos;
+                    if(Vector3.Dot(enemyDirection, transform.forward) < 1.5)
+                    {
+                        Debug.Log("Hit Enemy & Applied Damage");
+                        nearEnemy[i].GetComponent<EnemyController>().AdjustCurrentHealth(-10);    
+                    }
+                }
+            }
+        }
+    }
+    void OnTriggerEnter(Collider col)
+    {
+
+        if (col.gameObject.tag == "Enemy")
+        {
+            nearEnemy.Add(col.gameObject);
+        }
+    }
+
+    void OnTriggerExit(Collider col)
+    {
+        if (col.gameObject.tag == "Enemy")
+        {
+            nearEnemy.Remove(col.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/projectileController.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class projectileController : MonoBehaviour {
+
+    float lifespan = 3.0f;
+    float targetAlpha = 1.0f;
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        lifespan -= Time.deltaTime;
+
+        if (lifespan <= 0)
+        {
+            Disappear();
+        }
+	}
+
+    void OnCollisionEnter()
+    {
+       
+    }
+
+    void Disappear(){
+        float fadeTime = 0.0f;
+
+        Color c = gameObject.GetComponent<Renderer>().material.color;
+        float currentAlpha = c.a;
+        int count = 0;
+        while (fadeTime <= 1)
+        {
+            c.a = Mathf.Lerp(currentAlpha, targetAlpha, fadeTime);
+            gameObject.GetComponent<Renderer>().material.color= c;
+            fadeTime += Time.deltaTime / lifespan;
+            Debug.Log(fadeTime);
+            count++;
+            if (count == 10)
+            {
+                break;
+            }
+        }
+        c.a = targetAlpha;
+        gameObject.GetComponent<Renderer>().material.color = c;
+        Destroy(gameObject);
+    }
+}
